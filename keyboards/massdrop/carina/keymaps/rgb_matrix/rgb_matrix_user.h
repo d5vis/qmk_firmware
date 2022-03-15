17bd42ca8ca02cfdd0cebcd498629de196e7a34d
pbrizardo.gmail.com <pbrizardo.gmail.com>
2022-03-15 00:08:05 -0700

    Added back purple border with white per key

--- keyboards/massdrop/carina/keymaps/rgb_matrix/rgb_matrix_user.h
+++ keyboards/massdrop/carina/keymaps/rgb_matrix/rgb_matrix_user.h
@@ -2,6 +2,15 @@
   #ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE
     #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
+static HSV CUSTOM_REACTIVE(HSV hsv, uint16_t offset) {
+    hsv.s = 0;
+    uint8_t valoff = 60;
+    uint8_t val = scale8(255 - offset, hsv.v) + valoff;
+    hsv.v = val> 255 ? 255 : val;
+    return hsv;
+}
+
+
 static HSV CUSTOM_SOLID_REACTIVE(HSV hsv, uint16_t offset) {
     hsv.v = scale8(255 - offset, hsv.v);
     return hsv;
